9db18c2ef388ebcbf18822192efb25a655515223
Todkar <mrunal-sunilkumar.todkar@capgemini.com>
2019-05-17 14:47:55 +0530

    BankApp jdbc mvc

---
+++ src/com/capgemini/bankapp/mvc/controller/BankAccountController.java
@@ -0,0 +1,10 @@
+package com.capgemini.bankapp.mvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/bankapp")
+public class BankAccountController {
+
+}
